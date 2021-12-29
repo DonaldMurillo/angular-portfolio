234a739c62d38ebf791ed0c968ec7d5cbc7ca786
ManuelGCV <manuel_cardona@hotmail.es>
2021-12-28 23:03:29 -0500

    - Installed swagger - Updated user and user-profile dtos to use swagger - Created swagger config document
    Co-authored-by: Donald Murillo <DonaldMurillo@users.noreply.github.com>

--- apps/api/src/app/modules/user-profile/dto/create-user-profile.dto.ts
+++ apps/api/src/app/modules/user-profile/dto/create-user-profile.dto.ts
@@ -1,8 +1,14 @@
 import { IsAlphanumeric, IsBoolean, IsNotEmpty, IsOptional, IsString, Length } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserProfileDto {
-
+  @ApiProperty({
+    description: 'is alphanumeric',
+    required: true,
+    format: 'string',
+    minLength: 4,
+    maxLength: 20
+  })
 	@IsNotEmpty()
 	@IsAlphanumeric()
 	@Length(4, 20)
@@ -11,10 +17,22 @@ export class CreateUserProfileDto {
 	//@IsOptional()
 	//avatarImage: Buffer
 
+  @ApiProperty({
+    description: 'Allows trades to be published globaly',
+    required: false,
+    format: 'boolean'
+
+  })
 	@IsOptional()
 	@IsBoolean()
 	showTrades: boolean
 
+  @ApiProperty({
+    description: 'Set theme to be applied on user profile',
+    required: false,
+    format: 'string'
+
+  })
 	@IsOptional()
 	@IsString()
 	theme: string

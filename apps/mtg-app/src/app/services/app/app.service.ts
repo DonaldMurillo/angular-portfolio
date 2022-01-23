01f4756063a6cf062d746e74027cf68ee51d2254
DonaldMurillo <donald.murillo07@gmail.com>
2022-01-22 20:27:03 -0500

    misc fixes

--- apps/mtg-app/src/app/services/app/app.service.ts
+++ apps/mtg-app/src/app/services/app/app.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs/operators';
 import { AppTheme } from './app.model';
 import { AppStore } from './app.store';
 import { UserService } from '../user/user.service';
@@ -18,7 +17,7 @@ export class AppService {
 	 */
 	toggleTheme(theme?: AppTheme) {
 		const userState = this.userQuery.getValue();
-		if (theme === userState.theme) return;
+		if (theme && theme === userState.theme) return;
 		if (userState.id) {
 			this.userService.updateProfile({theme: theme ?? userState.theme === 'dark' ? 'light' : 'dark'}, true);
 			this.appStore.update(state => ({ ...state, theme: theme ?? (userState.theme === 'dark' ? 'light' : 'dark')}));
